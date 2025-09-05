ea151797de19a3f38628c3b5ea04447faca582ad
TWF <2192074454@qq.com>
2025-09-05 23:16:05 +0800

    提交

---
+++ day1/04数据类型.py
@@ -0,0 +1,5 @@
+print(type("TWF"))
+print(type(666))
+print(type(3.14))
+
+# 变量无类型,有类型的是变量存储的数据
---
+++ day1/05数据类型转换.py
@@ -0,0 +1,5 @@
+# 将数字类型转成字符串
+num = 123
+str_num = str(num)
+print(str_num)  # 输出: '123'
+print(type(str_num))  # 输出: <class 'str'>
