1151bbb1fd7fc90d06c6216af30d506bbbe28ca7
TWF <2192074454@qq.com>
2025-09-07 23:07:03 +0800

    提交

--- day1/05数据类型转换.py
+++ day1/05数据类型转换.py
@@ -1,5 +1,25 @@
 # 将数字类型转成字符串
 num = 123
-str_num = str(num)
-print(str_num)  # 输出: '123'
-print(type(str_num))  # 输出: <class 'str'>
+num_str = str(num)
+print(num_str)  # 输出: '123'
+print(type(num_str)) # 输出: <class 'str'>
+
+# 将字符串类型转成数字
+num_int = int(num_str)
+print(num_int)  # 输出: 123
+print(type(num_int)) # 输出: <class 'int'>
+
+# 将整数类型转成浮点数
+num_float = float(num_int)
+print(num_float)  # 输出: 123.0
+print(type(num_float)) # 输出: <class 'float'>
+
+# 将浮点数类型转成整数,会舍弃小数部分
+num_int2 = int(num_float)
+print(num_int2)  # 输出: 123
+print(type(num_int2)) # 输出: <class 'int'>
+
+# 所有类型都可以转成字符串,但不是所有字符串都能转成数字
+str = "hello"
+str_int = int(str)
+print(str_int)  # 报错: ValueError: invalid literal for int() with base 10: 'hello'
---
+++ day1/08字符串.py
@@ -0,0 +1,30 @@
+# 字符串有三种定义方法
+# 单引号
+name = 'TWF'
+print(name)
+# 双引号
+name = "TWF"
+print(name)
+# 三引号
+name = """
+我是
+TWF
+"""
+print(name)
+
+# 单引号包含双引号
+name = '"我是TWF"'
+print(name)
+# 双引号包含单引号
+name = "'我是TWF'"
+print(name)
+# 三引号也可以,还能换行
+name = """
+我是
+TWF
+"""
+print(name)
+
+# 也可以用转义字符转移
+name = '\'我是TWF\''
+print(name)
