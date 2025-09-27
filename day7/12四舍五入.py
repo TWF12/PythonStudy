29928a3de22225403df73ef031179dd90b2180c3
TWF <2192074454@qq.com>
2025-09-27 23:34:33 +0800

    提交

---
+++ day7/12四舍五入.py
@@ -0,0 +1,12 @@
+print(round(3.5)) # 4
+print(round(3.51)) # 4
+
+print(round(4.5)) # 4
+print(round(4.51)) # 5
+
+print(round(-3.50)) # -4
+
+print(round(-3.51)) # -4
+
+print(round(-20.51)) # -21
+print(round(-20.50)) # -20
---
+++ day7/13Union类型.py
@@ -0,0 +1,6 @@
+from typing import Union
+
+mylist: list[Union[int, str]] = [123, "itheima", "ticast"]
+
+def func(data: Union[int, str]) -> Union[int, str]:
+    pass
