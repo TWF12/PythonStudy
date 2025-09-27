aaa4121445ccbb5986d32d5372b314cb7d979aa8
TWF <2192074454@qq.com>
2025-09-28 07:59:35 +0800

    提交

---
+++ day7/14多态.py
@@ -0,0 +1,27 @@
+# 抽象类
+class Animal:
+    # 抽象方法
+    def speak(self):
+        pass
+
+
+class Dog(Animal):
+    def speak(self):
+        print("汪汪汪")
+
+
+class Cat(Animal):
+    def speak(self):
+        print("喵喵喵")
+
+
+def make_noise(animal: Animal):
+    animal.speak()
+
+
+dog = Dog()
+cat = Cat()
+
+
+make_noise(dog)
+make_noise(cat)
