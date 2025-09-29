229b7bce914dfa08d6843c5922a12553e4f57c59
TWF <2192074454@qq.com>
2025-09-29 12:03:32 +0800

    提交

--- computer_operation/test_2/4.14.2杨辉三角.py
+++ computer_operation/test_2/4.14.2杨辉三角.py
@@ -1,7 +1,23 @@
+def get_pascals_triangle(n: int):
+    # 数据
+    triangle = []
 
-for i in range(1, 6):
-    for k in range(5-i):
-        print('', end='  ')
-    for j in range(1, i + 1):
-        print(1, end='   ')
+    # 将每行数据初始化为1
+    for i in range(n):
+        row = [1] * ( i + 1)
+        # 计算中间元素的值
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+    return triangle # list[list]
+
+n = int(input("请输入要打印杨辉三角的行数: "))
+
+triangle = get_pascals_triangle(n)
+
+for i, row in enumerate(triangle):
+    for k in range(n-i-1):
+        print('', end='\t')
+    for col in row:
+        print(col, end='\t\t')
     print()
